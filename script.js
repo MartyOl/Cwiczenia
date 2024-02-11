9c91e1a32148d8b2f500990906ba32222bb3459a
Martyna O <121486221+MartyOl@users.noreply.github.com>
2024-02-11 09:59:04 -0800

    Add files via upload
    komenda for

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-let x = 30
-let y = 5
-let z = 0
-
-if (x!=y && x!=z && y!=z) {
-    if (x>y && x>z) {
-        console.log("Największa wartość to " + x)
-    }
-    if (y>x && y>z) console.log("Największa wartość to " + y)
-    if (z>x && z>y) console.log("Największa wartość to " + z)
-}
-
-
-//switch (x) {
-//    case 1:
-//    case 2:
-//    case 3:
-//        if (x==1) console.log("x wynosi 1")
-//       else if (x==2) console.log("x wynosi 2")
-//        else console.log("x wynosi 3")
-//        break;
-//   case 4:
-//        console.log("x wynosi 4");
-//        break;
-//     case 5:
-//       console.log("x wynosi 5");
-//       break;
-//   default:
-//        console.log("Żadna z powyższych opcji nie została wybrana")
+//for (i = 0; i<10; i++) {
+//    console.log("kolejne przejścia pętli " + i)
 //}
+
+for (let i = 2; i<=20 ;i+=2){
+    console.log(i)
+}
