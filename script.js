440615f8be06b17b60b3f4d4ea03043f49f903e5
Martyna O <121486221+MartyOl@users.noreply.github.com>
2024-02-13 08:30:17 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-let x = 30
-let y = 5
-let z = 0
-
-if (x!=y && x!=z && y!=z) {
-    if (x>y && x>z) {
-        console.log("Największa wartość to " + x)
-    }
-    if (y>x && y>z) console.log("Największa wartość to " + y)
-    if (z>x && z>y) console.log("Największa wartość to " + z)
-}
-
-
-//switch (x) {
-//    case 1:
-//    case 2:
-//    case 3:
-//        if (x==1) console.log("x wynosi 1")
-//       else if (x==2) console.log("x wynosi 2")
-//        else console.log("x wynosi 3")
-//        break;
-//   case 4:
-//        console.log("x wynosi 4");
-//        break;
-//     case 5:
-//       console.log("x wynosi 5");
-//       break;
-//   default:
-//        console.log("Żadna z powyższych opcji nie została wybrana")
+//let i = 0;
+//while (i<=10){
+//    console.log("kolejne przejście " + i);
+//    i++
 //}
+
+//let a = parseInt(prompt("podaj pierwszą liczbę"));
+//let b = parseInt(prompt("podaj drugą liczbę"));
+
+let a = 0;
+let b = 0;
+
+let suma = 0;
+
+while (suma>=0) {
+    let a = parseInt(prompt("podaj pierwszą liczbę"));
+    let b = parseInt(prompt("podaj drugą liczbę"));
+    suma = a + b;
+    console.log("Suma podanych liczb wynosi " + suma);
+}
